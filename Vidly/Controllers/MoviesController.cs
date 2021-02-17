c95552b7ec5ce9aa5eae50eb6778a2949b119173
bdoshen <bradley.doshen@gmail.com>
2021-02-17 16:53:09 -0500

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+     public class MoviesController : Controller
+     {
+          public ViewResult Index()
+          {
+               var movies = GetMovies();
+
+               return View(movies);
+          }
+
+          private IEnumerable<Movie> GetMovies()
+          {
+               return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+          }
+
+          // GET: Movies/Random
+          public IActionResult Random()
+          {
+               var movie = new Movie() { Name = "Shrek!" };
+               return View(movie);
+          }
+     }
+}
